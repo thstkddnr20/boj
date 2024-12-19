b5e2b2d226143cc85dccb1989f7e91c9146fe2c5
손상욱 <thstkddnr20@naver.com>
2024-12-19 13:38:58 +0900

    2차원 배열 정렬

---
+++ src/boj11650/Main.java
@@ -0,0 +1,34 @@
+package boj11650;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (p1, p2) -> {
+            if(p1[0] == p2[0])  {
+                return p1[1] - p2[1];
+            }
+            else {
+                return p1[0] - p2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
