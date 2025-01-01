f88c4ce3e5828ca239033edf144ffabe3903891f
thstkddnr20 <thstkddnr20@naver.com>
2025-01-01 12:35:04 +0900

    nqueen 푸는중,,

--- src/boj9663/Main.java
+++ src/boj9663/Main.java
@@ -3,21 +3,26 @@ package boj9663;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     static int N;
     static int[][] board; //체스판
-    static boolean[][] visited; //체스판에 말을 놓을 수 있는가
-    static boolean[][] prevVisited; //전 재귀에서 막힌 곳
+    static boolean[][] placed; //체스판에 말을 놓을 수 있는가(체스판을 되돌릴때 중복되는 부분이 있어 문제가 됨) -> Queen이 놓여진 자리로 변경
     static int result = 0;
+
+    static int dx[] = { 1, -1, 1, -1 };
+    static int dy[] = { 1, 1, -1, -1 };
+
+    static int sx[] = { 1, -1, 0, 0 };
+    static int sy[] = { 0, 0, 1, -1 };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
         board = new int[N][N];
-        visited = new boolean[N][N];
-        prevVisited = new boolean[N][N];
+        placed = new boolean[N][N];
 
         find(0);
         System.out.println(result);
@@ -29,92 +34,52 @@ public class Main {
             return;
         }
 
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) { //말을 놓을 수 있다면
-                    visited[i][j] = true; //말을 놓고
-                    prevVisited[i][j] = true;
+                if (possible(i, j, number)) { //queen이 놓여져있지 않고, 다른 queen에 의해 잡히지 않는 곳이라면
+                    placed[i][j] = true; //queen을 놓고
 
-                    //말이 이동가능한 방향을 모두 막는다
-                    //상하좌우
-                    for (int k = 0; k < N; k++) {
-                        if (!visited[k][j]) {
-                            visited[k][j] = true;
-                            prevVisited[k][j] = true;
-                        }
-                        if (!visited[i][k]) {
-                            visited[i][k] = true;
-                            prevVisited[i][k] = true;
-                        }
-                    }
-                    //대각선
-                    diagonal(i, j);
-
-                    //막은 후 number을 증가시켜 재귀
+                    //queen을 놓고 number을 증가시켜 재귀
                     find(number + 1);
 
-                    //막은 부분을 다시 풀어놓는다 -> 이 부분에서 문제 발생 그 전에 건드린 보드판만 지워야 하는데 다른것도 지워짐
-                    for (int k = 0; k < N; k++) {
-                        for (int l = 0; l < N; l++) {
-                            if (prevVisited[k][l]) {
-                                visited[k][l] = false;
-                                prevVisited[k][l] = false;
-                            }
-                        }
-                    }
-
+                    placed[i][j] = false; //놓은 queen을 되돌린다
                 }
             }
         }
 
     }
 
-    private static void diagonal(int i, int j) {
-        int currentX = i;
-        int currentY = j;
-        while (currentX >= 0 && currentX < N && currentY >= 0 && currentY < N) {
-            if (!visited[currentX][currentY]) {
-                visited[currentX][currentY] = true;
-                prevVisited[currentX][currentY] = true;
-            }
-            currentX = currentX - 1;
-            currentY= currentY + 1;
+    static boolean possible(int x, int y, int queenNum) {
+        //queen이 놓여진 체스판 placed와 x, y를 비교하여 말을 놓을 수 있는 자리인지 파악
+        if (queenNum == 0) { //놓여진 queen이 없다면
+            return true;
         }
 
-        //우상
-        currentX = i;
-        currentY = j;
-        while (currentX >= 0 && currentX < N && currentY >= 0 && currentY < N) {
-            if (!visited[currentX][currentY]) {
-                visited[currentX][currentY] = true;
-                prevVisited[currentX][currentY] = true;
+        while (x >= 0 && x < N && y >= 0 && y < N) {
+            for (int i = 0; i < N; i++) {
+                if (placed[x][i]) {
+                    return false;
+                }
             }
-            currentX = currentX + 1;
-            currentY= currentY + 1;
+
+            for (int i = 0; i < N; i++) {
+                if (placed[i][y]) {
+                    return false;
+                }
+            }
+
+            //대각
+            for (int i = 0; i < N; i++) {
+                if (placed[x - i][y - i]) {
+
+                }
+            }
         }
 
-        //좌하
-        currentX = i;
-        currentY = j;
-        while (currentX >= 0 && currentX < N && currentY >= 0 && currentY < N) {
-            if (!visited[currentX][currentY]) {
-                visited[currentX][currentY] = true;
-                prevVisited[currentX][currentY] = true;
-            }
-            currentX = currentX - 1;
-            currentY= currentY - 1;
-        }
+        
+        //queen과 같은 x, y면 안됨
+        //queen과 대각에 놓여있으면 안됨
 
-        //우하
-        currentX = i;
-        currentY = j;
-        while (currentX >= 0 && currentX < N && currentY >= 0 && currentY < N) {
-            if (!visited[currentX][currentY]) {
-                visited[currentX][currentY] = true;
-                prevVisited[currentX][currentY] = true;
-            }
-            currentX = currentX + 1;
-            currentY= currentY - 1;
-        }
     }
 }
